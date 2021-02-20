8f2cb419bb15b7986d8ba5da061908681f710dd5
clitetailor <ducnhatphung@gmail.com>
2021-02-20 17:54:47 +0700

    Add coming soon promotion banner

---
+++ snowpack.config.js
@@ -0,0 +1,5 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: { public: '/', src: '/dist' },
+  plugins: ['@snowpack/plugin-svelte'],
+}
---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<!-- App.svelte -->
+<script>
+  /* component logic will go here */
+</script>
+
+<div class="height-100">
+  <header
+    class="
+      height-100
+      flex
+      flex-column
+      justify-content-center
+      align-items-center
+    "
+  >
+    <h1 class="display-title-xxl">The Compiler</h1>
+
+    <p class="font-size-xl">Comming Soon!</p>
+  </header>
+</div>
